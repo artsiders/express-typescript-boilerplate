8ac26fbde9101f1f59e63a33403100cb6dd8ff39
Njikam Mohamed Salim <salim.artsider@gmail.com>
2024-01-21 16:32:52 +0100

    initial commit [a simple api with typescript and express]

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app =  express()
+
+app.get("/", (req, res) => {
+    return res.send("hello world !")
+})
+
+app.listen(3000, () => {
+    console.log("successfully !✨")
+})
